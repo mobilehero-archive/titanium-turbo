c66abb354e4bee0039bd714792e9591166e5a765
Brenton House <brenton.house@gmail.com>
2020-10-15 11:22:30 -0500

    🏁     v2.0.8 ➖    Releasing new version

--- Alloy/commands/compile/ast/fix-require.js
+++ Alloy/commands/compile/ast/fix-require.js
@@ -1,6 +1,7 @@
 const path = require('path');
 let config = {};
 let registry = [];
+const _ = require('lodash');
 
 module.exports = ({ types: t }) => {
 
@@ -15,6 +16,9 @@ module.exports = ({ types: t }) => {
 			registry = require(path.join(config.dir.resourcesPlatform, '__file_registry.json' ));
 			filepath = file.opts.filename;
 			if( filepath.startsWith(config.dir.resourcesPlatform)){
+				if( ! config.dir.resourcesPlatform ){
+					debugger;
+				}
 				filename = filepath.substring(config.dir.resourcesPlatform.length);
 				dirname = path.dirname(filename);
 			}
@@ -23,17 +27,50 @@ module.exports = ({ types: t }) => {
 		visitor: {
 			CallExpression: (_path, state) => {
 				const args = _path.get('arguments');
-				const callee = _path.get('callee')
+				const callee = _path.get('callee');
+				const first = args[0];
+
+				if( ! filepath && state.filename ){
+					filepath = state.filename;
+					if( filepath.startsWith(config.dir.resourcesPlatform)){
+						if( ! config.dir.resourcesPlatform ){
+							debugger;
+						}
+						filename = filepath.substring(config.dir.resourcesPlatform.length);
+						dirname = path.dirname(filename);
+					}
+				}
+
 				let targetPath = _path.node.arguments[0];
+
 				if ( t.isIdentifier(callee.node, { name: 'require' }) ) {
 
-					if (dirname && t.isStringLiteral(args[0])) {
-						const originalModulePath = args[0].node.value;
-						if( originalModulePath.startsWith('.')){
-							const modifiedModulePath = path.resolve(dirname, originalModulePath);
-							targetPath = t.stringLiteral(modifiedModulePath);
-						}
+					if( ! targetPath ){
+						debugger;
 					}
+
+					if( dirname ){
+						if ( t.isStringLiteral(first)) {
+							const originalModulePath = first.node.value;
+							if( originalModulePath.startsWith('.')){
+								const modifiedModulePath = path.resolve(dirname, originalModulePath);
+								targetPath = t.stringLiteral(modifiedModulePath);
+							}
+						} else if(t.isTemplateLiteral(first)) {
+
+							const originalModulePath = _.get(first, 'node.quasis[0].value.cooked');
+
+							if( _.get(first, 'node.expressions.length' )){
+								// Don't do anything here yet...
+							} else if( originalModulePath && originalModulePath.startsWith('.') ) {
+								const modifiedModulePath = path.resolve(dirname, originalModulePath);
+								// Not sure if this is the official way to do this but it seems to work
+								targetPath.quasis[0].value.raw = modifiedModulePath;
+								targetPath.quasis[0].value.cooked = modifiedModulePath;
+							} 
+						} 
+					}
+
 					_path.replaceWith(
 						t.callExpression(callee.node, [
 							targetPath,
